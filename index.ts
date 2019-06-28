ce3322583bd1355893873a4524e1528c46beeebc
Dom Henderson <Dominic.Henderson@softwire.com>
2019-06-28 09:20:29 +0100

    Part way through part 4

--- index.ts
+++ index.ts
@@ -1,82 +1,143 @@
 const request = require('request');
 const readline = require('readline-sync');
 const moment = require('moment');
+const express = require('express');
 
-function displayNextBuses(error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    let jsonData = JSON.parse(body);
-    
-    jsonData.sort((left, right) => moment(left.expectedArrival).unix() - moment(right.expectedArrival).unix());
-    jsonData = jsonData.slice(0,5);
-    for ( let bus of jsonData ) {
-        console.log(moment(bus.expectedArrival).format('HH:mm ddd'));
-    }
-}
-
-function getNextBusesAtBusStop ( id : string ) {
-    console.log(`id: ${id}`);
-    const url = `https://api.tfl.gov.uk/StopPoint/${id}/Arrivals`;
-    return new Promise ((resolve, reject) => {
-        console.log('promise started');
+function promiseRequest(url: string){
+    return new Promise((resolve,reject) => {
         request(url, ( error, response, body) => {
-            console.log('request started');
             if ( error == null ) {
-                let jsonData = JSON.parse(body);
-                console.log(jsonData);
-                jsonData.sort((left, right) => moment(left.expectedArrival).unix() - moment(right.expectedArrival).unix());
-                console.log(jsonData);
-                resolve ( jsonData );
+                resolve(body);
             } else {
-                reject ( error );
+                reject(error);
             }
-        } );
-    } );
+        });
+    });
 }
 
-function busesFromPostcode(error, response, body){
-    let jsonData = JSON.parse(body);
-    let longitude = jsonData.result.longitude;
-    let latitude = jsonData.result.latitude;
-    console.log(longitude);
-    console.log(latitude);
-    const radius = 200;
-    const url = `https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&radius=${radius}&lat=${latitude}&lon=${longitude}`;
-    return request ( url, function ( error, response, body) {
-        //console.log(body);
-        let stopPoints = JSON.parse(body).stopPoints;
-        stopPoints.sort((left,right)=>left.distance-right.distance);
-        let firstTwo = stopPoints.slice(0,2);
+function getNextBusesAtBusStop ( id : string, count?: number ): Promise<any> {
+    //console.log(`id: ${id}`);
+    const url = `https://api.tfl.gov.uk/StopPoint/${id}/Arrivals`;
+    return promiseRequest(url).then(
+        (body: string) => {
+            let jsonData = JSON.parse(body);
+            //console.log(jsonData);
+            jsonData.sort((left, right) => moment(left.expectedArrival).unix() - moment(right.expectedArrival).unix());
+            //console.log(jsonData);
+            if (count != undefined){
+                jsonData = jsonData.slice(0,count)
+            }
+            return jsonData;
+        },
+        (error: string) => {
+            console.log(error);
+        }
+        
+    );
+}
 
-        let allBusPromises = firstTwo.map((stopPointData) => getNextBusesAtBusStop(stopPointData.id));
-        Promise.all(allBusPromises)
-            .then(nextBusesData => {
-                console.log(firstTwo);
-                console.log(nextBusesData);
-                for ( let i in firstTwo ) {
-                    console.log(`Buses at ${firstTwo[i].commonName}:`);
-                    for ( let bus of nextBusesData[i] ) {
-                        console.log(`   ${moment(bus.expectedArrival).format('HH:mm ddd')}`);
-                    }
-                    console.log('');
+function printBusesFromPostcode(postcode: string){
+    const url = `https://api.postcodes.io/postcodes/${postcode}`
+
+    return promiseRequest ( url ).then (
+        ( body: string ) => {
+            let jsonData = JSON.parse(body);
+            let longitude = jsonData.result.longitude;
+            let latitude = jsonData.result.latitude;
+            const radius = 200;
+            const url = `https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&radius=${radius}&lat=${latitude}&lon=${longitude}`;
+            promiseRequest ( url ).then (
+                ( body: string ) => {
+                    let busStops = JSON.parse(body).stopPoints;
+                    busStops.sort((left,right)=>left.distance-right.distance);
+                    let nearestBusStops = busStops.slice(0,2);
+
+                    let allBusPromises = nearestBusStops.map((stopPointData) => getNextBusesAtBusStop(stopPointData.id, 5));
+                    Promise.all(allBusPromises).then(nextBusesData => {
+                        //console.log(firstTwo);
+                        //console.log(nextBusesData);
+                        for ( let i in nearestBusStops ) {
+                            printBusTimes(nearestBusStops[i], nextBusesData[i]);
+                        }
+                    });
+                },
+                ( error: string ) => {
+                    console.log(error);
                 }
-            })
-    } );
+            )
+        },
+        ( error: string ) => {
+            console.log(error);
+        }
+    );
+}
+
+function getBusesJSONFromPostcode(postcode: string){
+    const url = `https://api.postcodes.io/postcodes/${postcode}`
+
+    return promiseRequest( url ).then (
+        ( body: string ) => {
+            let jsonData = JSON.parse(body);
+            let longitude = jsonData.result.longitude;
+            let latitude = jsonData.result.latitude;
+            const radius = 200;
+            return `https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&radius=${radius}&lat=${latitude}&lon=${longitude}`;
+        }, ()=>console.log('AAAAAAAAAAAAAAAAH')
+    )
+    .then(promiseRequest, ()=>console.log('AAAAAAAAH'))
+    .then(handleStopPointsResponse, ()=>console.log('AAAAAAAAH'));
+}
+
+function handleStopPointsResponse(apiResponseBody) {
+    let busStops = JSON.parse(apiResponseBody).stopPoints;
+    busStops.sort((left, right) => left.distance - right.distance);
+    let nearestBusStops = busStops.slice(0,2);
+
+    let allBusPromises = nearestBusStops.map((stopPointData) => getNextBusesAtBusStop(stopPointData.id, 5));
+    return Promise.all(allBusPromises).then(nextBusesData => {
+        //console.log(firstTwo);
+        //console.log(nextBusesData);
+        let jsonData = [];
+        for ( let i in nearestBusStops ) {
+            // printBusTimes(nearestBusStops[i], nextBusesData[i]);
+            jsonData.push({
+                name: nearestBusStops[i].commonName,
+                times: []
+            });
+            for ( let bus of nextBusesData[i] ) {
+                jsonData[i].times.push(moment(bus.expectedArrival).format('HH:mm ddd'));
+            }
+        }
+        return jsonData;
+    });
+}
+
+function printBusTimes(busStop, busTimes) {
+    console.log(`Buses at ${busStop.commonName}:`);
+    for (let bus of busTimes) {
+        console.log(`   ${moment(bus.expectedArrival).format('HH:mm ddd')}`);
+    }
+    console.log('');
 }
 
 function main() {
-    console.log("Enter a Postcode: ");
-    //const response = readline.prompt();
-    const response = 'NW51TL';
-    const url = `https://api.postcodes.io/postcodes/${response}`
-    request(url, busesFromPostcode)
-    // console.log('Enter a stop code');
+    // console.log("Enter a Postcode: ");
     // //const response = readline.prompt();
-    // const response = '490008660N';
-    // const url = `https://api.tfl.gov.uk/StopPoint/${response}/Arrivals`;
-    // request(url, displayNextBuses );
+    // const response = 'NW51TL';
+    // return getBusesJSONFromPostcode(response).then(
+    //     jsonData => console.log(jsonData)
+    // );
+    
+    const app = express();
+    const port = 3000;
+
+    app.get('/departureBoards', (req, res) => {
+        let postcode = req.query.postcode;
+        console.log(postcode);
+        getBusesJSONFromPostcode ( postcode ).then ( res.json, ()=>console.log('AAAAAH') );
+    } );
+
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 }
-
-
 
 main();
